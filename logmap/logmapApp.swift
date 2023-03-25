2ac8d66f2e79dcd4a6a38968d2b5b4980f9c3b65
吉田裕哉 <yuu@yoshi.local>
2023-03-25 15:38:38 +0900

    first commit

---
+++ logmap/logmapApp.swift
@@ -0,0 +1,17 @@
+//
+//  logmapApp.swift
+//  logmap
+//
+//  Created by 吉田裕哉 on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct logmapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
